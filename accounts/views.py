193b3aa7201e966aab55bebbb8165c1f13138ed8
Sbtha <shisastore@protonmail.com>
2021-09-26 20:04:40 +0200

    Modified login and register html templates

--- accounts/views.py
+++ accounts/views.py
@@ -8,47 +8,55 @@ from django.contrib.auth.decorators import login_required
 
 def register_user(request):
 
-    form = CustomUserCreationForm()
+    if request.user.is_authenticated:
+        return redirect('core:dashboard')
 
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+    else:
+        form = CustomUserCreationForm()
 
-            messages.success(request, (f'User: {username} created.'))
-            return redirect('accounts:login')
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
 
-    return render(request, 'accounts/register_user.html', {
+                messages.success(request, (f'User: {username} created.'))
+                return redirect('accounts:login')
 
-        'form': form,
+        return render(request, 'accounts/register_user.html', {
 
-    })
+            'form': form,
+
+        })
 
 
 def login_user(request):
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('core:dashboard')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            messages.success(request, (f'{username}, you were logged in.'))
-            return redirect('core:dashboard')
-        else:
-            messages.info(request, ('Wrong login or password.'))
+            if user is not None:
+                login(request, user)
+                messages.success(request, (f'{username}, you were logged in.'))
+                return redirect('core:dashboard')
+            else:
+                messages.info(request, ('Wrong login or password.'))
 
-    return render(request, 'accounts/login_user.html', {
+        return render(request, 'accounts/login_user.html', {
 
-    })
+        })
 
 
 def logout_user(request):
 
-    logout(request)
-    messages.success(
-        request, ('You Were Logged Out.'))
-    return redirect('accounts:login')
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(
+            request, ('You Were Logged Out.'))
+        return redirect('accounts:login')
